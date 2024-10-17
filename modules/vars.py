0948be792f562cd6f9daea6851a583942457493b
𝕯𝖗𝖆𝖌𝖔𝖓🐉 <dragonex7778@gmail.com>
2024-10-17 17:07:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24515547"
 api_hash = "0809699a061f02fbbcc1a6c4443547ee"
-bot_token = "7372223228:AAGHOzfIP76yhMeugaTxSRqI9OK7jjg4gDk"
+bot_token = "8122278384:AAHR005qMz99eDmxJfyg4vCjt4rQRkbYvxE"
