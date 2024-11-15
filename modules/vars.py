ae50a4f03004da1c1723b5643ffc8670878f0af9
𝕯𝖗𝖆𝖌𝖔𝖓🐉 <dragonex7778@gmail.com>
2024-11-15 22:17:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24515547"
 api_hash = "0809699a061f02fbbcc1a6c4443547ee"
-bot_token = "8138754480:AAHZKerxsZdMO68gWw3HdEYTIimlTlzwMr8"
+bot_token = "7648568266:AAGknrttNTrr8H0UDnu0xqYtZ-N8_UsD3Ck"
